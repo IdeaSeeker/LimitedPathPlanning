07bcb38e09890b202a0ae3280f2b41b74f19efd2
IdeaSeeker <stroganov.n.36@gmail.com>
2021-12-14 01:04:50 +0300

    add map change

---
+++ src/Change.py
@@ -0,0 +1,11 @@
+class Change:
+
+    def __init__(self, time: int, i: int, j: int, is_obst: bool):
+        self._time = time
+        self._i = i
+        self._j = j
+        self._is_obst = is_obst
+
+
+    def coordinates(self):
+        return self._i, self._j
