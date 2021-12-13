3b1ed71dbafa01476f0550b10db57b7c5774429e
IdeaSeeker <stroganov.n.36@gmail.com>
2021-12-14 01:06:27 +0300

    add d star

--- src/Map.py
+++ src/Map.py
@@ -1,4 +1,6 @@
-from src import Node
+from Node import Node
+from Change import Change
+from utils import *
 
 
 class Map:
@@ -9,12 +11,60 @@ class Map:
         self._cells = grid_cells
 
 
-    def read_from_string(self, cell_str, width, height):
+    def __getitem__(self, node):
+        return self._cells[node.i][node.j]
+
+
+    def __setitem__(self, node, value):
+        self._cells[node.i][node.j] = value
+
+
+    def __str__(self):
+        result = []
+        for i in range(self._width):
+            for j in range(self._height):
+                if self._cells[i][j] is None:
+                    result.append('#')
+                else:
+                    result.append('.')
+            result.append('\n')
+        return ''.join(result)
+
+
+    def update_node(self, node):
+        self[node] = node
+
+
+    def print(self, verbose = False):
+        if verbose:
+            for i in range(self._width):
+                for j in range(self._height):
+                    print(self._cells[i][j], end = ' ')
+        else:
+            print(str(self))
+
+
+    def print_path(self, path: list[Node]):
+        result = str(self).split('\n')
+        result = list(map(list, result))
+        for node in path:
+            if result[node.i][node.j] == '#':
+                result[node.i][node.j] = '@'
+            else:
+                result[node.i][node.j] = 'O'
+        for line in result:
+            print(''.join(line))
+
+
+    def read_from_string(self, width, height, cell_str):
         # '.' - for free cells, any other - for obstacles
         self._width = width
         self._height = height
         self._cells = [
-            [0 for _ in range(width)]
+            [
+                None  # for obstacles
+                for _ in range(width)
+            ]
             for _ in range(height)
         ]
 
@@ -24,7 +74,8 @@ class Map:
                 continue
             j = 0
             for cell in line:
-                self._cells[i][j] = int(cell != '.')  # 0 for free cells
+                if cell == '.':  # Node() for free cells
+                    self._cells[i][j] = Node(i, j)
                 j += 1
             i += 1
 
@@ -32,7 +83,17 @@ class Map:
     def init_nodes(self):
         for i in range(self._width):
             for j in range(self._height):
-                self._cells[i][j] = Node(i, j, g = 10 ** 9)
+                if not self._cells[i][j] is None:
+                    self._cells[i][j].g   = inf
+                    self._cells[i][j].rhs = inf
+
+
+    def apply_change(self, change: Change):
+        i, j = change.coordinates()
+        if change._is_obst:
+            self._cells[i][j] = None
+        else:
+            self._cells[i][j] = Node(i, j, g = inf, rhs = inf)
 
 
     def is_on_grid(self, i, j):
@@ -40,10 +101,12 @@ class Map:
 
 
     def is_free(self, i, j):
-        return self.is_on_grid(i, j) and self._cells[i][j] == 0
+        return self.is_on_grid(i, j) and not self._cells[i][j] is None
 
 
-    def get_neighbors(self, i, j, diagonal_allowed = False):
+    def get_neighbors(self, node, diagonal_allowed = False):
+        i, j = node.coordinates()
+
         if not self.is_free(i, j):
             return []
 
