731b540db2322d75cec63ec0a396853ba54698e0
Dmitrii <dhkas@mail.ru>
2021-12-23 03:26:47 +0300

    iterative LPAStar

--- src/LPAStar.py
+++ src/LPAStar.py
@@ -7,8 +7,7 @@ from utils import *
 
 
 class LPAStar:
-
-    def __init__(self, map: Map, start: Node, finish: Node, changes: List[List[Change]]):
+    def __init__(self, map: Map, start: Node, finish: Node):
         self._map = map
 
         self._start = start
@@ -25,18 +24,22 @@ class LPAStar:
         self._map.update_node(self._start)
 
         self._open = OpenList()
-        self._open[self._start] = self.calculate_key(self._start)
+        self._open.insert(self.calculate_key(self._start), self._start)
 
-        self._changes = changes
+        self.compute_shortest_path()
 
 
     def calcutale_rhs(self, s):
         if s == self._start:
             return 0
-        rhs = min(
-            adj_node.g + compute_cost(adj_node, s)
-            for adj_node in self._map.get_neighbors(s)
-        )
+        neighbors = self._map.get_neighbors(s)
+        if len(neighbors) > 0:
+            rhs = min(
+                adj_node.g + compute_cost(adj_node, s)
+                for adj_node in neighbors
+            )
+        else:
+            rhs = inf
         return min(rhs, inf)
 
 
@@ -50,17 +53,19 @@ class LPAStar:
             s.rhs = self.calcutale_rhs(s)
             self._map.update_node(s)
         if s in self._open:
-            self._open.pop(s)
+            self._open.remove(s)
         if s.g != s.rhs:
-            self._open[s] = self.calculate_key(s)
+            self._open.insert(self.calculate_key(s), s)
 
 
     def compute_shortest_path(self):
+        visited = []
         while not self._open.is_empty() and (
             self._open.get_min_value() < self.calculate_key(self._finish) or \
             self._finish.rhs != self._finish.g
         ):
-            u, _ = self._open.pop()
+            u, _ = self._open.pop_min_value()
+            visited.append(u)
             if u.g > u.rhs:
                 u.g = u.rhs
                 for s in self._map.get_neighbors(u):
@@ -70,6 +75,7 @@ class LPAStar:
                 for s in self._map.get_neighbors(u) + [u]:
                     self.update_vertex(s)
             self._map.update_node(u)
+        return visited
 
 
     def print_path(self):
@@ -78,6 +84,8 @@ class LPAStar:
 
     def greedy_path(self):
         current = self._map[self._finish]
+        if current.g == inf:
+            return None
         path = [current]
         while current != self._start and current.g < inf:
             current = min(self._map.get_neighbors(current), key=lambda node: node.g)
@@ -85,43 +93,20 @@ class LPAStar:
         return list(reversed(path))
 
 
-    def run(self):
-        self.compute_shortest_path()
-        self.print_path(self.greedy_path())
-        for change_set in self._changes:
-            for change in change_set:
-                self._map.apply_change(change)
-                i, j = change.coordinates
-                new_node = self._map[Node(i, j)]
-                if new_node is not None:
-                    self.update_vertex(new_node)
-                else:
-                    for adj_ij in self._map.get_neighbors(Node(i, j), free_required = False):
-                        self.update_vertex(adj_ij)
+    def apply_changes(self, changes: List[Change]):
+        for change in changes:
+            self._map.apply_change(change)
+            i, j = change.coordinates
+            new_node = self._map[Node(i, j)]
+            if new_node is not None:
+                self.update_vertex(new_node)
+            else:
+                for adj_ij in self._map.get_neighbors(Node(i, j), free_required = False):
+                    self.update_vertex(adj_ij)
 
-                for s in self._open._data:
-                    self._open._data[s] = self.calculate_key(s)
+            for s in list(self._open._data.keys()):
+                if s in self._open:
+                    self._open.remove(s)
+                self._open.insert(self.calculate_key(s), s)
 
-            self.compute_shortest_path()
-            self._map.print_path(self.greedy_path())
-
-
-if __name__ == "__main__":
-    _map = Map()
-    _map.read_from_string(3, 3,
-'''
-...
-#..
-#..
-''')
-    _start = Node(0, 0)
-    _finish = Node(2, 2)
-    _changes = [
-        [Change(1, 1, 1, True)],
-        [Change(1, 1, 2, True),
-        Change(10, 1, 0, False),
-        Change(10, 2, 0, False)]
-    ]
-
-    d_star = LPAStar(_map, _start, _finish, _changes)
-    d_star.run()
+        return self.greedy_path(), self.compute_shortest_path()
